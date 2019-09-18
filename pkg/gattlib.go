08f2bc0929ec38d9b4469b13d7e907ae538c1981
pavel m <paullus325@gmail.com>
2019-09-18 11:59:10 +0300

    sync

--- pkg/gattlib.go
+++ pkg/gattlib.go
@@ -54,6 +54,7 @@ func (g *Gatt) Read(uuid string) ([]byte, int, error) {
 	}
 
 	ptr := unsafe.Pointer(&buffer[0])
+	ptr = unsafe.Pointer(ptr)
 	res := C.gattlib_read_char_by_uuid(g.conn, &uuidS.uuid, &ptr, &n)
 	if res != 0 {
 		return nil, 0, GattError(int(res))
