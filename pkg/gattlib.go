89029830392a1a408afa4e5baccde0f2adfb535e
pavel m <paullus325@gmail.com>
2019-09-18 12:06:55 +0300

    sync

--- pkg/gattlib.go
+++ pkg/gattlib.go
@@ -45,20 +45,18 @@ func (g *Gatt) Disconnect() error {
 }
 
 func (g *Gatt) Read(uuid string) ([]byte, int, error) {
-	buffer := make([]byte, 100)
 	var n C.size_t
-	n = 100
 	uuidS, err := g.uUID(uuid)
 	if err != nil {
 		return nil, 0, err
 	}
 	var ptr unsafe.Pointer
-	//ptr := unsafe.Pointer(&buffer[0])
-	//ptr = unsafe.Pointer(ptr)
 	res := C.gattlib_read_char_by_uuid(g.conn, &uuidS.uuid, &ptr, &n)
 	if res != 0 {
 		return nil, 0, GattError(int(res))
 	}
+
+	buffer := C.GoBytes(unsafe.Pointer(*(**C.uchar)(ptr)), C.int(n))
 	return buffer, int(n), nil
 }
 
